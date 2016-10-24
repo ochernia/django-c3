4f2d55c7b2cdb5833ef30f8d95e75f3da836da7f
Paulo <commonzenpython@gmail.com>
2016-10-24 13:50:20 -0400

    Bumped version to 0.0.4

--- django_c3/__init__.py
+++ django_c3/__init__.py
@@ -3,4 +3,4 @@ django_c3 aims to make model translation easy and is designed to let you use
 the built-in Django features (Query API, Model Forms, Admin, etc) as intended.
 """
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
